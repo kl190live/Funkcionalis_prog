6f4e8f8fd6ee097e0648d47642663933a4ff0061
Petrik Diak <diak@petrik.hu>
2023-11-06 15:57:36 +0100

    Az idézetet megcsinaltam mar csak a gomb hianyzik

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,21 @@
 import './style.css'
+import {quotes} from'./quotes.json'
+
+document.addEventListener('DOMContentLoaded',()=>{
+  console.log("Összes idézet")
+  const idezet=Array.from(quotes.map((quotes)=>quotes.author+" : "+"„"+quotes.quote+"”"))
+  idezet.sort((a,b)=>{ 
+  if (a < b) {
+    return -1;
+  }
+  if (a > b) {
+    return 1;
+  }
+  return 0;
+  });
+  console.log(idezet)
+})
+
+
+
 
