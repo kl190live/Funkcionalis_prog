aa9caccedfd4019474bb14cdff57a94cfa755f4e
Petrik Diak <diak@petrik.hu>
2023-11-06 17:10:46 +0100

    A tobbi feladathoz is megcsinaltam a gombokat

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ document.addEventListener('DOMContentLoaded',()=>{
   Feladat_1.textContent="Összes idézet"
   document.getElementById('app')!.appendChild(Feladat_1)
     Feladat_1.addEventListener('click',()=>{
-      const idezet=Array.from(quotes.map((quotes)=>quotes.author+" : "+"„"+quotes.quote+"”"))
+      const lista_1=document.getElementById('lista_1')
+      const idezet=Array.from(quotes.map((quotes)=>quotes.author+" : "+quotes.quote))
       idezet.sort((a,b)=>{ 
       if (a < b) {
         return -1;
@@ -16,7 +17,15 @@ document.addEventListener('DOMContentLoaded',()=>{
       }
       return 0;
       });
-      console.log(idezet)
+      idezet.forEach((item)=>{
+        let szoveg=item.split(': ')
+        let quote=document.createElement('blockquote')
+        let li=document.createElement('li')
+        li.textContent=szoveg[0];
+        quote.textContent=szoveg[1];
+        li.appendChild(quote)
+        lista_1!.appendChild(li)
+      })
     })
   const Feladat_2=document.createElement('button')
   Feladat_2.textContent="The"
