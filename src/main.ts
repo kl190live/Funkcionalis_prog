e1fe4a53639807e7383a5a3debcabab44ca24751
Petrik Diak <diak@petrik.hu>
2023-11-06 16:50:36 +0100

    A gombokat megcsinaltam a feladathoz

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,46 @@ import './style.css'
 import {quotes} from'./quotes.json'
 
 document.addEventListener('DOMContentLoaded',()=>{
-  console.log("Összes idézet")
-  const idezet=Array.from(quotes.map((quotes)=>quotes.author+" : "+"„"+quotes.quote+"”"))
-  idezet.sort((a,b)=>{ 
-  if (a < b) {
-    return -1;
-  }
-  if (a > b) {
-    return 1;
-  }
-  return 0;
-  });
-  console.log(idezet)
+  const Feladat_1=document.createElement('button')
+  Feladat_1.textContent="Összes idézet"
+  document.getElementById('app')!.appendChild(Feladat_1)
+    Feladat_1.addEventListener('click',()=>{
+      const idezet=Array.from(quotes.map((quotes)=>quotes.author+" : "+"„"+quotes.quote+"”"))
+      idezet.sort((a,b)=>{ 
+      if (a < b) {
+        return -1;
+      }
+      if (a > b) {
+        return 1;
+      }
+      return 0;
+      });
+      console.log(idezet)
+    })
+  const Feladat_2=document.createElement('button')
+  Feladat_2.textContent="The"
+  document.getElementById('app')!.appendChild(Feladat_2)
+    Feladat_2.addEventListener('click',()=>{
+      
+      const The=Array.from(quotes.map((quotes)=>"„"+quotes.quote+"”"))
+      const the=The.find((the)=>the==="The","the")
+      
+      console.log(The)
+    })
+  const Feladat_3=document.createElement('button')
+  Feladat_3.textContent="Hossz"
+  document.getElementById('app')!.appendChild(Feladat_3)
+  document.getElementById('app')!.appendChild(Feladat_3)
+    Feladat_3.addEventListener('click',()=>{
+      
+
+    })
+  const Feladat_4=document.createElement('button')
+  Feladat_4.textContent="Darabszám"
+  document.getElementById('app')!.appendChild(Feladat_4)
+  document.getElementById('app')!.appendChild(Feladat_4)
+    Feladat_4.addEventListener('click',()=>{
+      
+
+    })
 })
-
-
-
-
